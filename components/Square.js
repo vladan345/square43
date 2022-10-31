4f34d579a3fc75131baffd422c1aa6c3c8a05363
vladan345 <vladanm087@gmail.com>
2022-10-31 13:57:45 +0100

    fixed about, fixed home

--- components/Square.js
+++ components/Square.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import styles from "../styles/component-css/Square.module.css";
 
 function Square(props) {
+  const square = useRef();
+  useEffect(() => {
+    const element = square.current;
+    switch (props.state) {
+      case 1:
+        element.style.background = "red";
+        break;
+      case 2:
+        element.style.background = "green";
+        break;
+      case 3:
+        element.style.background = "yellow";
+        break;
+      case 4:
+        element.style.background = "blue";
+        break;
+      case 5:
+        element.style.background = "pink";
+        break;
+      case 6:
+        element.style.background = "orange";
+        break;
+    }
+  }, [props.state]);
   return (
-    <div className={styles.square}>
-      <Image
+    <div className={styles.square} ref={square}>
+      {/* <Image
         src={props.image}
         alt="Square 43 studio"
         width={560}
         height={props.height}
         priority
-      />
+      /> */}
     </div>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,84 +1,63 @@
-import dynamic from "next/dynamic";
-const Animator = dynamic(
-  import("react-scroll-motion").then((it) => it.Animator),
-  { ssr: false }
-);
-import {
-  ScrollContainer,
-  ScrollPage,
-  batch,
-  Fade,
-  MoveIn,
-  MoveOut,
-  Sticky,
-} from "react-scroll-motion";
-
 import React from "react";
-import Image from "next/image";
 import styles from "../styles/About.module.css";
 
+import { MotionAnimate } from "react-motion-animate";
+
 function About() {
-  const FadeUp = batch(Fade(), MoveIn(0, 200), MoveOut(0, -500), Sticky());
   return (
     <div className={styles.About}>
       <div className="wrapper">
-        <ScrollContainer>
-          <ScrollPage>
-            <Animator animation={FadeUp}>
-              <p className={styles.statement}>
-                <span className={styles.gradientText}>
-                  Building is believing
-                </span>{" "}
-                🙏 - and we&apos;ll build a bright future in front of you.
-              </p>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage>
-            <Animator animation={FadeUp}>
-              <p className={styles.statement}>
-                See your brand take its{" "}
-                <span className={styles.gradientText}>first breath.</span> 🐣
-              </p>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage>
-            <Animator animation={FadeUp}>
-              <p className={styles.statement}>
-                Word your <span className={styles.gradientText}>copy well</span>{" "}
-                🔤 - it speaks a thousand words.
-              </p>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage>
-            <Animator animation={FadeUp}>
-              <p className={styles.statement}>
-                Digital experience beyond your{" "}
-                <span className={styles.lineThrough}>expectations</span>{" "}
-                <span className={styles.gradientText}>imagination.</span> 💭
-              </p>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage>
-            <Animator animation={FadeUp}>
-              <p className={styles.statement}>
-                Socialize and get in{" "}
-                <span className={styles.gradientText}>touch</span> 👉👈 with
-                your audience.
-              </p>
-            </Animator>
-          </ScrollPage>
-          <ScrollPage>
-            <Animator animation={FadeUp}>
-              <p className={styles.statement}>
-                Get your shop - there is a line in front, but{" "}
-                <span className={styles.gradientText}>
-                  you&apos;re next up!
-                </span>{" "}
-                😀
-              </p>
-            </Animator>
-          </ScrollPage>
-        </ScrollContainer>
+        <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
+          <div style={{ height: "100vh", paddingTop: "100px" }}>
+            <p className={styles.statement}>
+              <span className={styles.gradientText}>Building is believing</span>{" "}
+              🙏 - and we&apos;ll build a bright future in front of you.
+            </p>
+          </div>
+        </MotionAnimate>
+        <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
+          <div style={{ height: "100vh" }}>
+            <p className={styles.statement}>
+              See your brand take its{" "}
+              <span className={styles.gradientText}>first breath.</span> 🐣
+            </p>
+          </div>
+        </MotionAnimate>
+        <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
+          <div style={{ height: "100vh" }}>
+            <p className={styles.statement}>
+              Word your <span className={styles.gradientText}>copy well</span>{" "}
+              🔤 - it speaks a thousand words.
+            </p>
+          </div>
+        </MotionAnimate>
+        <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
+          <div style={{ height: "100vh" }}>
+            <p className={styles.statement}>
+              Digital experience beyond your{" "}
+              <span className={styles.lineThrough}>expectations</span>{" "}
+              <span className={styles.gradientText}>imagination.</span> 💭
+            </p>
+          </div>
+        </MotionAnimate>
+        <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
+          <div style={{ height: "100vh" }}>
+            <p className={styles.statement}>
+              Socialize and get in{" "}
+              <span className={styles.gradientText}>touch</span> 👉👈 with your
+              audience.
+            </p>
+          </div>
+        </MotionAnimate>
+        <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
+          <div style={{ height: "100vh" }}>
+            <p className={styles.statement}>
+              Get your shop - there is a line in front, but{" "}
+              <span className={styles.gradientText}>you&apos;re next up!</span>{" "}
+              😀
+            </p>
+          </div>
+        </MotionAnimate>
       </div>
     </div>
   );
