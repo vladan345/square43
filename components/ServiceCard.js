96acfbb178516087e5e2678a1bc9ea9531c96a74
vladan345 <vladanm087@gmail.com>
2022-11-09 15:24:32 +0100

    services fix

--- components/ServiceCard.js
+++ components/ServiceCard.js
@@ -4,6 +4,7 @@ import styles from "../styles/component-css/ServiceCard.module.css";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useLoading } from "../utils/hooks/LoadingContext";
+import { InView } from "react-intersection-observer";
 
 function ServiceModule(props) {
   const router = useRouter();
@@ -18,34 +19,44 @@ function ServiceModule(props) {
     }, 1000);
   };
   return (
-    <Link
-      onClick={handleClick}
-      href={"/services/" + props.service.id}
-      className={styles.ServiceModule}
-    >
-      <div className={styles.overlay}></div>
-      <div className={styles.top}>
-        <h2 className={styles.title}>{props.service.name}</h2>
-        <ul className={styles.list}>
-          {props.service.list.map((listItem, key) => (
-            <li className={styles.listItem} key={key}>
-              {listItem}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className={styles.link}>
-        view projects{" "}
-        <div className={styles.icon}>
-          <Image
-            src="/images/arrow-white-r.svg"
-            alt="arrow icon right"
-            width={40}
-            height={40}
-          />
-        </div>
-      </div>
-    </Link>
+    <>
+      <InView as="div" threshold={1}>
+        {({ inView, ref }) => (
+          <div ref={ref}>
+            <Link
+              onClick={handleClick}
+              href={"/services/" + props.service.id}
+              className={`${styles.ServiceModule} ${
+                inView ? styles.open : null
+              }`}
+            >
+              <div className={styles.overlay}></div>
+              <div className={styles.top}>
+                <h2 className={styles.title}>{props.service.name}</h2>
+                <ul className={styles.list}>
+                  {props.service.list.map((listItem, key) => (
+                    <li className={styles.listItem} key={key}>
+                      {listItem}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.link}>
+                view projects{" "}
+                <div className={styles.icon}>
+                  <Image
+                    src="/images/arrow-white-r.svg"
+                    alt="arrow icon right"
+                    width={40}
+                    height={40}
+                  />
+                </div>
+              </div>
+            </Link>
+          </div>
+        )}
+      </InView>
+    </>
   );
 }
 
