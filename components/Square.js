566e4dac42b2382acc7cfc5c3f361f7c85a08fa2
vladan345 <vladanm087@gmail.com>
2022-11-14 12:26:38 +0100

    fallback

--- components/Square.js
+++ components/Square.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import styles from "../styles/component-css/Square.module.css";
-import { useLoading } from "../utils/hooks/LoadingContext";
+
 function Square(props) {
-  const { setLoading } = useLoading();
-  let loadedCount = 1;
   const square = useRef();
   useEffect(() => {
     const element = square.current;
@@ -32,86 +30,35 @@ function Square(props) {
         break;
     }
   }, [props.state]);
-  setLoading(true);
-  const checkLoaded = () => {
-    if (loadedCount !== 6) {
-      loadedCount += 1;
-      console.log(loadedCount);
-    } else {
-      setLoading(false);
-    }
-  };
   return (
     <div className={styles.square} ref={square}>
       <div className={`${styles.videoWrap} video1`}>
-        <video
-          onLoadedData={checkLoaded()}
-          autoPlay
-          playsInline
-          muted
-          loop
-          className={styles.video}
-        >
+        <video autoPlay playsInline muted loop className={styles.video}>
           <source src="/images/square1.mp4" type="video/mp4" />
         </video>
       </div>
       <div className={`${styles.videoWrap} video2`}>
-        <video
-          autoPlay
-          playsInline
-          muted
-          loop
-          className={styles.video}
-          onLoadedData={checkLoaded()}
-        >
+        <video autoPlay playsInline muted loop className={styles.video}>
           <source src="/images/square2.mp4" type="video/mp4" />
         </video>
       </div>
       <div className={`${styles.videoWrap} video3`}>
-        <video
-          autoPlay
-          playsInline
-          muted
-          loop
-          className={styles.video}
-          onLoadedData={checkLoaded()}
-        >
+        <video autoPlay playsInline muted loop className={styles.video}>
           <source src="/images/square3.mp4" type="video/mp4" />
         </video>
       </div>
       <div className={`${styles.videoWrap} video4`}>
-        <video
-          autoPlay
-          playsInline
-          muted
-          loop
-          className={styles.video}
-          onLoadedData={checkLoaded()}
-        >
+        <video autoPlay playsInline muted loop className={styles.video}>
           <source src="/images/square4.mp4" type="video/mp4" />
         </video>
       </div>
       <div className={`${styles.videoWrap} video5`}>
-        <video
-          autoPlay
-          playsInline
-          muted
-          loop
-          className={styles.video}
-          onLoadedData={checkLoaded()}
-        >
+        <video autoPlay playsInline muted loop className={styles.video}>
           <source src="/images/northprim/Northprim-hero.mp4" type="video/mp4" />
         </video>
       </div>
       <div className={`${styles.videoWrap} video6`}>
-        <video
-          autoPlay
-          playsInline
-          muted
-          loop
-          className={styles.video}
-          onLoadedData={checkLoaded()}
-        >
+        <video autoPlay playsInline muted loop className={styles.video}>
           <source src="/images/square6.mp4" type="video/mp4" />
         </video>
       </div>
