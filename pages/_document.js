cf4e955054c1142fda590f8df9fc7e8baa2627a6
vladan345 <vladanm087@gmail.com>
2022-11-30 13:27:08 +0100

    linkedin fix

--- pages/_document.js
+++ pages/_document.js
@@ -44,8 +44,18 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script dangerouslySetInnerHTML='_linkedin_partner_id = "4929457"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id);' />
-          <Script dangerouslySetInnerHTML='(function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(window.lintrk);' />
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `_linkedin_partner_id = "4929457"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id);`,
+            }}
+          />
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(window.lintrk);`,
+            }}
+          />
           <noscript>
             <img
               height="1"
