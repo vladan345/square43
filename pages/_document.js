3650eb766bbee43f4194904c64a9e13cef0dc682
vladan345 <vladanm087@gmail.com>
2022-11-30 13:38:09 +0100

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -46,7 +46,7 @@ class MyDocument extends Document {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5S6TZ4F');</script>`,
+})(window,document,'script','dataLayer','GTM-5S6TZ4F');`,
             }}
           />
         </Head>
