b9407c3e4028fe1d52b924afc687835f40cbcaef
vladan345 <vladanm087@gmail.com>
2023-07-17 08:11:30 +0200

    fixed about page

--- pages/inside.js
+++ pages/inside.js
@@ -1,9 +1,58 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/About.module.css";
 import Head from "next/head";
-import { MotionAnimate } from "react-motion-animate";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 function About() {
+  const main = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      let sections = gsap.utils.toArray("section");
+      const sectionPos = main.current.getBoundingClientRect();
+      gsap.set(".trigger", { height: sections.length * 200 + "vh" });
+      let currentSection;
+      currentSection = sections[0];
+
+      sections.forEach((section, i) => {
+        var tl = gsap.timeline({
+          overwrite: true,
+          scrollTrigger: {
+            start: () => (i - 0.5) * innerHeight * 2 + sectionPos.top,
+            end: () => (i + 0.5) * innerHeight * 2 + sectionPos.top,
+            onToggle: (self) => {
+              return self.isActive && setSection(section);
+            },
+          },
+        });
+
+        function setSection(newSection) {
+          if (newSection !== currentSection) {
+            var tl = gsap.timeline();
+            tl.to(currentSection, {
+              autoAlpha: 0,
+              y: 100,
+              duration: 0.5,
+              overwrite: true,
+            });
+            tl.to(newSection, {
+              autoAlpha: 1,
+              y: 0,
+              duration: 0.5,
+              overwrite: true,
+            });
+
+            currentSection = newSection;
+          }
+        }
+      }, main.current);
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
     <>
       <Head>
@@ -35,56 +84,51 @@ function About() {
           content="https://square43.com/images/About.png"
         />
       </Head>
-      <main>
+      <main ref={main}>
         <div className={styles.About}>
-          <div className="wrapper">
-            <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
-              <div style={{ height: "200vh", paddingTop: "100px" }}>
+          <div className="wrapper trigger">
+            <div className={`${styles.stickyWrap} `}>
+              <section className="statementSection" style={{ height: "200vh" }}>
                 <p className={styles.statement}>
                   <span className={styles.gradientText}>
                     Building is believing
                   </span>{" "}
                   🙏 - and we&apos;ll build a bright future in front of you.
                 </p>
-              </div>
-            </MotionAnimate>
-            <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
-              <div style={{ height: "200vh" }}>
+              </section>
+
+              <section className="statementSection" style={{ height: "200vh" }}>
                 <p className={styles.statement}>
                   See your brand take its{" "}
                   <span className={styles.gradientText}>first breath.</span> 🐣
                 </p>
-              </div>
-            </MotionAnimate>
-            <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
-              <div style={{ height: "200vh" }}>
+              </section>
+
+              <section className="statementSection" style={{ height: "200vh" }}>
                 <p className={styles.statement}>
                   Word your{" "}
                   <span className={styles.gradientText}>copy well</span> 🔤 - it
                   speaks a thousand words.
                 </p>
-              </div>
-            </MotionAnimate>
-            <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
-              <div style={{ height: "200vh" }}>
+              </section>
+
+              <section className="statementSection" style={{ height: "200vh" }}>
                 <p className={styles.statement}>
                   Digital experience beyond your{" "}
                   <span className={styles.lineThrough}>expectations</span>{" "}
                   <span className={styles.gradientText}>imagination.</span> 💭
                 </p>
-              </div>
-            </MotionAnimate>
-            <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
-              <div style={{ height: "200vh" }}>
+              </section>
+
+              <section className="statementSection" style={{ height: "200vh" }}>
                 <p className={styles.statement}>
                   Socialize and get in{" "}
                   <span className={styles.gradientText}>touch</span> 👉👈 with
                   your audience.
                 </p>
-              </div>
-            </MotionAnimate>
-            <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
-              <div style={{ height: "200vh" }}>
+              </section>
+
+              <section className="statementSection" style={{ height: "200vh" }}>
                 <p className={styles.statement}>
                   Get your shop - there is a line in front, but{" "}
                   <span className={styles.gradientText}>
@@ -92,17 +136,16 @@ function About() {
                   </span>{" "}
                   😀
                 </p>
-              </div>
-            </MotionAnimate>
-            <MotionAnimate speed={0.7} reset={true} animation="fadeInUp">
-              <div style={{ height: "200vh" }}>
+              </section>
+
+              <section className="statementSection" style={{ height: "200vh" }}>
                 <p className={styles.statement}>
                   Why go in circles? <br />
                   Go
                   <span className={styles.gradientText}> Square (43)</span>
                 </p>
-              </div>
-            </MotionAnimate>
+              </section>
+            </div>
           </div>
         </div>
       </main>
