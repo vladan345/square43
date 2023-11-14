fe7f564ab294384d5d5d90946cf9d405c60729f8
Vladan <57448109+vladan345@users.noreply.github.com>
2023-11-14 13:20:58 +0100

    Proj/klei (#14)
    * init klei
    
    * klei desktop
    
    * removed section
    
    * init klei mobile
    
    * fixed images on klei
    
    * finished klei

---
+++ pages-sections/klei/Flowers.js
@@ -0,0 +1,118 @@
+import { useRef, useEffect } from "react";
+import styles from "@/styles/section-css/klei/Flowers.module.css";
+import Image from "next/image";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Flowers() {
+  const main = useRef();
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to(".circle", {
+        stagger: 0.3,
+        duration: 0.5,
+        opacity: 1,
+        delay: 1,
+        scrollTrigger: {
+          trigger: ".wrapper",
+          start: "center bottom",
+        },
+      });
+
+      gsap.to(".circle", {
+        duration: 5,
+        rotate: 360,
+        ease: "none",
+        repeat: -1,
+        stagger: 0.3,
+      });
+
+      gsap.to(".flower", {
+        duration: 1,
+        opacity: 1,
+        bottom: 0,
+        scrollTrigger: {
+          trigger: ".wrapper",
+          start: "center bottom",
+        },
+      });
+    }, main.current);
+
+    return () => ctx.revert();
+  }, []);
+  return (
+    <section className={styles.Flowers} ref={main}>
+      <Image
+        src="/images/klei/back.webp"
+        alt="background"
+        fill
+        className={styles.back}
+      />
+      <div className={`wrapper ${styles.row}`}>
+        <div className={styles.col}>
+          <h2>
+            Approach
+            <br />& Philosophy
+          </h2>
+          <p>
+            “There can be no single approach to art or beauty.”
+            <br />
+            <br />
+            The copywriting behind the brand aimed to explain that no two Klei
+            products are alike. Considering the multifaceted nature of beauty,
+            this brand transcends the mere act of production, embodying an
+            artistic and philosophical approach to craftsmanship. In fact, each
+            clay product is meticulously fashioned as an intimate dialogue
+            between the raw earth and artisans' vision.
+          </p>
+          <p>
+            Klei Works is a testament to the alchemy of clay, where artistry and
+            philosophy converge to mold objects that resonate with the soul.
+          </p>
+        </div>
+        <div className={styles.col}>
+          <div className={styles.circles}>
+            <Image
+              src="/images/klei/krug1.svg"
+              alt="circle"
+              width={414}
+              height={414}
+              className="circle"
+            />
+            <Image
+              src="/images/klei/krug2.svg"
+              alt="circle"
+              width={565}
+              height={555}
+              className="circle"
+            />
+            <Image
+              src="/images/klei/krug3.svg"
+              alt="circle"
+              width={652}
+              height={710}
+              className="circle"
+            />
+          </div>
+          <Image
+            src="/images/klei/flower.webp"
+            alt="flower girl"
+            width={684}
+            height={1045}
+            className={`${styles.flower} flower`}
+          />
+        </div>
+      </div>
+      <Image
+        src="/images/klei/front.webp"
+        alt="front"
+        fill
+        className={styles.front}
+      />
+    </section>
+  );
+}
---
+++ pages-sections/klei/Laptop.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import styles from "@/styles/section-css/klei/Laptop.module.css";
+
+export default function Laptop() {
+  return (
+    <section className={styles.Laptop}>
+      <div className="wrapper">
+        <h2>
+          Web Design
+          <br /> & Development
+        </h2>
+        <div className={styles.textRow}>
+          <p>
+            Digital presence has become an indispensable step in the development
+            of a new brand. Returning to stage 1, we aimed to apply the ideal of
+            minimalism and all things artistic: grainy, even smudged textures,
+            breathtaking visuals of top-tier pottery - everything that is an
+            irreplaceable part of the pottery creation process.
+          </p>
+          <p>
+            In terms of web development, we ensured that the website was
+            presented in all its glory and uniqueness for all device types.
+            Recognizing the critical importance of an adaptable interface in
+            today's dynamic online ecosystem, our team undertook a comprehensive
+            approach to ensure optimal user experience across various devices.
+            Klei Works is now available for desktop, tablet and mobile.
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.background}>
+        <Image
+          src="/images/klei/laptop_bg.webp"
+          alt="Background laptop"
+          fill
+          className={styles.laptopBg}
+        />
+        <Image
+          src="/images/klei/laptop.webp"
+          alt="Laptop"
+          width={1279}
+          height={1007}
+          className={styles.laptopImage}
+        />
+        <video playsInline autoPlay muted loop className={styles.video}>
+          <source src="/images/klei/laptop-video.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </section>
+  );
+}
