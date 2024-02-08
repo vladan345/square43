d3cabdb479f4c5173fe6b435f9445358e938a875
vladan345 <vladanm087@gmail.com>
2024-02-08 12:39:49 +0100

    updated gogaudi videos

--- app/projects/(single projects)/gogaudi/ui/Videos.js
+++ app/projects/(single projects)/gogaudi/ui/Videos.js
@@ -4,53 +4,40 @@ import Image from "next/image";
 export default function Videos() {
   return (
     <section className={styles.Videos}>
-      <div className={styles.tile}>
-        <Image
-          src="/images/gogaudi/person_1.webp"
-          alt="GoGaudi senior"
-          width={436}
-          height={470}
-        />
-      </div>
-      <div className={styles.tile}>
-        <Image
-          src="/images/gogaudi/person_2.webp"
-          alt="GoGaudi senior"
-          width={638}
-          height={489}
-        />
-      </div>
-      <div className={styles.tile}>
-        <Image
-          src="/images/gogaudi/person_3.webp"
-          alt="GoGaudi senior"
-          width={514}
-          height={470}
-        />
-      </div>
-      <div className={styles.tile}>
-        <Image
-          src="/images/gogaudi/person_4.webp"
-          alt="GoGaudi senior"
-          width={471}
-          height={498}
-        />
-      </div>
-      <div className={styles.tile}>
-        <Image
-          src="/images/gogaudi/person_5.webp"
-          alt="GoGaudi senior"
-          width={592}
-          height={503}
-        />
-      </div>
-      <div className={styles.tile}>
-        <Image
-          src="/images/gogaudi/person_6.webp"
-          alt="GoGaudi senior"
-          width={476}
-          height={481}
-        />
+      <video autoPlay playsInline muted loop className={styles.video}>
+        <source src="/images/gogaudi/desktop.mp4" type="video/mp4" />
+      </video>
+      <div className={styles.videoGrid}>
+        <div className={styles.tile}>
+          <video autoPlay playsInline muted loop>
+            <source src="/images/gogaudi/mobile1.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className={styles.tile}>
+          <video autoPlay playsInline muted loop>
+            <source src="/images/gogaudi/mobile2.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className={styles.tile}>
+          <video autoPlay playsInline muted loop>
+            <source src="/images/gogaudi/mobile3.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className={styles.tile}>
+          <video autoPlay playsInline muted loop>
+            <source src="/images/gogaudi/mobile4.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className={styles.tile}>
+          <video autoPlay playsInline muted loop>
+            <source src="/images/gogaudi/mobile5.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div className={styles.tile}>
+          <video autoPlay playsInline muted loop>
+            <source src="/images/gogaudi/mobile6.mp4" type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   );
