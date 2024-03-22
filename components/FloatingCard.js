27105970f408f645f3a1bf77382784f7950959e0
vladan345 <vladanm087@gmail.com>
2024-03-22 15:31:37 +0100

    service on home page

--- components/FloatingCard.js
+++ components/FloatingCard.js
@@ -3,51 +3,43 @@ import styles from "./styles/FloatingCard.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function FloatingCard(props) {
+export default function FloatingCard({ service }) {
   return (
-    <div>
-      <div
-        className={styles.main}
-        style={{
-          top: props.service.top,
-          left: props.service.left,
-        }}
-      >
-        <div className={styles.gradient}></div>
+    <div className={styles.main}>
+      <div className={styles.gradient}></div>
 
-        <div className={styles.textbox}>
-          <h2
-            dangerouslySetInnerHTML={{ __html: props.service.title }}
-            className={styles.title}
-          />
-          <div className={styles.contentWrapper}>
-            <ul className={styles.list}>
-              {props.service.subservices.map((subservice) => (
-                <li key={subservice}>{subservice}</li>
-              ))}
-            </ul>
-            <Link
-              href={"/services" + props.service.link}
-              className={styles.projectsArrowWrapper}
-            >
-              <div className={styles.viewProjects}>VIEW PROJECTS</div>
-              <div className={styles.button}>
-                <Image
-                  src="/images/arrow-white-r.svg"
-                  alt="button-box"
-                  height={40}
-                  width={40}
-                />
-              </div>
-            </Link>
-            <div className={styles.buttonBox}>
+      <div className={styles.textbox}>
+        <h2
+          dangerouslySetInnerHTML={{ __html: service.name }}
+          className={styles.title}
+        />
+        <div className={styles.contentWrapper}>
+          <ul className={styles.list}>
+            {service.features.map((feature) => (
+              <li key={feature}>{feature}</li>
+            ))}
+          </ul>
+          <Link
+            href={"/services" + service.slug.current}
+            className={styles.projectsArrowWrapper}
+          >
+            <div className={styles.viewProjects}>VIEW PROJECTS</div>
+            <div className={styles.button}>
               <Image
-                src="/images/expandsquare6.svg"
+                src="/images/arrow-white-r.svg"
                 alt="button-box"
-                height={24}
-                width={24}
+                height={40}
+                width={40}
               />
             </div>
+          </Link>
+          <div className={styles.buttonBox}>
+            <Image
+              src="/images/expandsquare6.svg"
+              alt="button-box"
+              height={24}
+              width={24}
+            />
           </div>
         </div>
       </div>
