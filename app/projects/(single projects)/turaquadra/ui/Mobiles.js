c9bfb2e017e63b60e5ea41071b4daff4cf9cc38c
vladan345 <vladanm087@gmail.com>
2024-10-18 14:29:22 +0200

    push images and videos

--- app/projects/(single projects)/turaquadra/ui/Mobiles.js
+++ app/projects/(single projects)/turaquadra/ui/Mobiles.js
@@ -34,13 +34,23 @@ export default function Mobiles() {
           height={810}
           className="image"
         />
-        <Image
-          src="/images/turaquadra/phone2.png"
-          alt="Phone 2"
-          width={395}
-          height={810}
-          className="image"
-        />
+        <div className="image relative">
+          <Image
+            src="/images/turaquadra/phone-frame.png"
+            alt="Phone 2"
+            width={395}
+            height={810}
+          />
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="absolute left-1/2 top-1/2 w-[93%] -translate-x-1/2 -translate-y-1/2"
+          >
+            <source src="/images/turaquadra/reel.mp4" type="video/mp4" />
+          </video>
+        </div>
         <Image
           src="/images/turaquadra/phone3.png"
           alt="Phone 3"
