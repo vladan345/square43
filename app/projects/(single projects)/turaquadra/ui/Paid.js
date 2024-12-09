16fea44d9f4f2e1d59dc9852e65933a444510ef7
Julian Bogdan <djulianbogdan@gmail.com>
2024-12-09 09:06:29 +0100

    new TQ page started

--- app/projects/(single projects)/turaquadra/ui/Paid.js
+++ app/projects/(single projects)/turaquadra/ui/Paid.js
@@ -11,9 +11,19 @@ export default function Paid() {
   const main = useRef();
   useGSAP(
     () => {
-      gsap.from(".image", {
+      gsap.from(".luxury", {
         scrollTrigger: {
           trigger: ".container",
+          start: "top 70%",
+        },
+        y: -200,
+        autoAlpha: 0,
+        duration: 1.2,
+      });
+
+      gsap.from(".paid", {
+        scrollTrigger: {
+          trigger: ".paid",
           start: "top 70%",
         },
         x: -200,
@@ -25,28 +35,40 @@ export default function Paid() {
   );
   return (
     <section className="Paid" ref={main}>
-      <div className="container flex h-[840px] lg:h-auto lg:flex-col">
-        <div className="image relative w-1/2 lg:h-[600px] lg:w-full md:h-[80vw]">
+      <div className="container relative z-[0] h-[960px] max-h-[960px]">
+        <div className="absolute h-[960px] w-[100%]">
           <Image
             fill
-            sizes="50vw, max-width(767px) 100vw"
-            src="/images/turaquadra/paid.png"
+            src="/images/turaquadra/background.png"
             alt="Turaquadra"
             className="object-cover"
           />
         </div>
-        <div className="flex w-1/2 items-center justify-center px-[40px] lg:w-full lg:px-[20px] lg:pt-[60px]">
-          <div className="max-w-[520px]">
-            <h2>Paid Advertisement</h2>
-            <p>
-              Mastering the right tools goes a long way when it comes to
-              sponsored content. Setting up the right parameters in terms of age
-              range, geolocation and trending keywords allows us to target the
-              perfect audience and present them with the type of real estate
-              they are looking for. End result: a significant increase in the
-              number of leads.
-            </p>
-          </div>
+        <div className="luxury absolute top-[10%] z-[1] flex w-[100%] justify-center">
+          <h1 className="font-['Ivy-Presto'] text-[160px] font-[100] text-[#fff]">
+            LUXURY EVERLASTING
+          </h1>
+        </div>
+        <div className="absolute bottom-[0px] z-[2] h-[743px] w-[100%]">
+          <Image
+            fill
+            src="/images/turaquadra/background2.png"
+            alt="Turaquadra"
+            className="object-cover"
+          />
+        </div>
+      </div>
+      <div className="paid relative z-[5] mt-[-300px] flex w-1/2 justify-end">
+        <div className="w-[600px] rounded-[20px] bg-[#202B3F] px-[40px] py-[60px]">
+          <h2 className="mb-[60px] text-[#fff]">Paid Advertisement</h2>
+          <p className="text-[#fff]">
+            Mastering the right tools goes a long way when it comes to sponsored
+            content. Setting up the right parameters in terms of age range,
+            geolocation and trending keywords allows us to target the perfect
+            audience and present them with the type of real estate they are
+            looking for. End result: a significant increase in the number of
+            leads.
+          </p>
         </div>
       </div>
     </section>
